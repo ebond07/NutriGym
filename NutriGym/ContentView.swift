5180749da8c8dfcc566181b28597ca43467db76a
Evan Bond <evanbond007@outlook.com>
2023-10-18 16:50:21 -0400

    Initial Commit

---
+++ NutriGym/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NutriGym
+//
+//  Created by Evan Bond on 2023-10-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NutriGym/NutriGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriGymApp.swift
+//  NutriGym
+//
+//  Created by Evan Bond on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct NutriGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
