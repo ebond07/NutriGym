6be37f19c85353c4e81fa8d9d951e5f617c9179b
Evan Bond <evanbond007@outlook.com>
2023-10-30 16:05:16 -0400

    Styled navbar and added MVVM for Nutritional Plans + the button for it on the main page

--- NutriGym/ContentView.swift
+++ NutriGym/ContentView.swift
@@ -13,6 +13,14 @@ struct Client: Identifiable {
     var name: String
 }
 
+// Model
+struct NutritionalPlan: Identifiable {
+    var id: UUID
+    var name: String
+    var description: String
+}
+
+
 // View Model
 class ClientsViewModel: ObservableObject {
     @Published var clients: [Client]
@@ -27,12 +35,53 @@ class ClientsViewModel: ObservableObject {
     }
 }
 
+// View Model
+class NutritionalPlansViewModel: ObservableObject {
+    @Published var nutritionalPlans: [NutritionalPlan]
+
+    init() {
+        // Initialize or fetch nutritional plan data here
+        self.nutritionalPlans = [
+            NutritionalPlan(id: UUID(), name: "Plan 1", description: "Description for Plan 1"),
+            NutritionalPlan(id: UUID(), name: "Plan 2", description: "Description for Plan 2")
+            // Add more nutritional plans as needed
+        ]
+    }
+}
+
+struct NutritionalPlansView: View {
+    @ObservedObject var viewModel: NutritionalPlansViewModel
+
+    var body: some View {
+        List(viewModel.nutritionalPlans, id: \.id) { plan in
+            NavigationLink(destination: NutritionalPlanDetailView(plan: plan)) {
+                Text(plan.name)
+            }
+        }
+        .navigationBarTitle("Nutritional Plans")
+    }
+}
+
+struct NutritionalPlanDetailView: View {
+    let plan: NutritionalPlan
+
+    var body: some View {
+        VStack {
+            Text("Name: \(plan.name)")
+            Text("Description: \(plan.description)")
+            // Add more details about the nutritional plan
+        }
+        .navigationBarTitle(plan.name)
+    }
+}
+
+// View
 struct DashboardView: View {
     var body: some View {
         Text("Dashboard")
     }
 }
-
+// View
 struct ClientsView: View {
     @ObservedObject var viewModel: ClientsViewModel
     @State private var selectedClient: Client?
@@ -55,7 +104,7 @@ struct ClientsView: View {
         }
     }
 }
-
+// View
 struct SettingsView: View {
     var body: some View {
         Text("Settings")
@@ -64,6 +113,8 @@ struct SettingsView: View {
 
 struct ContentView: View {
     @StateObject var clientsViewModel = ClientsViewModel()
+    @StateObject var nutritionalPlansViewModel = NutritionalPlansViewModel()
+    @State private var showingNutritionalPlans = false
 
     var body: some View {
         TabView {
@@ -73,17 +124,41 @@ struct ContentView: View {
                         NavigationLink(destination: DashboardView()) {
                             Text("Dashboard")
                                 .padding(20)
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
                         }
                         NavigationLink(destination: ClientsView(viewModel: clientsViewModel)) {
                             Text("Clients")
                                 .padding(20)
+                                .background(Color.green)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
                         }
                         NavigationLink(destination: SettingsView()) {
                             Text("Settings")
                                 .padding(20)
+                                .background(Color.purple)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
                         }
                     }
                     Spacer()
+                    NavigationLink(
+                        destination: NutritionalPlansView(viewModel: nutritionalPlansViewModel),
+                        isActive: $showingNutritionalPlans,
+                        label: {
+                            Button(action: {
+                                showingNutritionalPlans = true
+                            }) {
+                                Text("Nutritional Plans")
+                                    .padding(20)
+                                    .background(Color.orange)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                        }
+                    )
                 }
                 .navigationBarTitle("", displayMode: .inline)
                 .navigationBarItems(leading: Text("NutriGym")
@@ -102,16 +177,25 @@ struct ContentView: View {
                     Text("Clients")
                 }
 
+//            NutritionalPlansView(viewModel: nutritionalPlansViewModel)
+//                .tabItem {
+//                    Image(systemName: "doc.text")
+//                    Text("Nutritional Plans")
+//                }
+
             SettingsView()
                 .tabItem {
                     Image(systemName: "gear")
                     Text("Settings")
                 }
+            
         }
     }
 }
 
+
 struct ClientDetailView: View {
+    @Environment(\.presentationMode) var presentationMode
     let client: Client
 
     var body: some View {
@@ -120,6 +204,15 @@ struct ClientDetailView: View {
             // Add more client information here (e.g., age, height, weight)
         }
         .navigationBarTitle(client.name)
+        .navigationBarItems(trailing: Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            Text("Close")
+                .padding(10)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        })
     }
 }
 
